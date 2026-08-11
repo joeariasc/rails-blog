8f555f546ac23784b8fa5c797132f42d2ae7ad6d
Joe Arias <jarias@wawand.co>
2026-08-11 10:31:27 -0500

    feat: refactor BlogPostsController to use before_action for set_blog_post, update routes to use resources

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,12 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-      redirect_to root_path
+    set_blog_post
   end
 
   def new
@@ -23,25 +23,33 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-      redirect_to root_path
+    set_blog_post
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
+    set_blog_post
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
       render :edit, status: :unprocessable_entity
     end
-  rescue ActiveRecord::RecordNotFound
-      redirect_to root_path
+  end
+
+  def destroy
+    set_blog_post
+    @blog_post.destroy
+    redirect_to root_path, status: :see_other
   end
 
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
   end
 end
